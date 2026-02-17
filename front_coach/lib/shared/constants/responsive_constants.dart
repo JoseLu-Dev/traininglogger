0fc7b6f73e946a24a6547569e1a7b04d0f40abbd
joseludev <joseludev@gmail.com>
2026-02-17 17:23:05 +0100

    made drawer smaller

--- front_coach/lib/shared/constants/responsive_constants.dart
+++ front_coach/lib/shared/constants/responsive_constants.dart
@@ -1,6 +1,6 @@
 class ResponsiveConstants {
   static const double tabletBreakpoint = 600.0;
-  static const double permanentDrawerWidth = 280.0;
+  static const double permanentDrawerWidth = 200.0;
 
   static bool isLargeScreen(double width) => width >= tabletBreakpoint;
 }
